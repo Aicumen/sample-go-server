32720d75497598e518eefc5d7aa74e15686526db
Akshay Meher <akshay@Akshays-MacBook-Pro.local>
2019-01-16 11:46:33 +0400

    swaggo integration

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/akshay111meher/sample-go-server/docs"
 	server "github.com/akshay111meher/sample-go-server/go"
 	"github.com/akshay111meher/sample-go-server/go/db"
 	keyMgmt "github.com/akshay111meher/sample-go-server/go/key_management"
+	"github.com/go-chi/chi"
 	"github.com/gorilla/handlers"
+	"github.com/swaggo/http-swagger"
 )
 
 func main() {
+	r := chi.NewRouter()
+
 	log.Printf("Server started")
+	r.Get("/swagger/*", httpSwagger.WrapHandler)
+
+	http.ListenAndServe(":8000", r)
+
 	keyMgmt.StartRedis()
 	db.StartMongo()
 	router := server.NewRouter()
